519498d4fd53e11839e4e39d0ab3ca078193dcc6
Nikita Leonov <nikita@buddyhopp.com>
2015-03-01 05:11:32 -0800

    SITE-1 | Initial commit.

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).foundation({
+  orbit: {
+    animation: 'fade',
+    timer_speed: 2000,
+    animation_speed: 500,
+    navigation_arrows: false,
+    bullets: false,
+    slide_number: false
+  }
+});
